2722096879e8c69a4e39809980f465c08e637c23
daniahsn <daniahsn@sas.upenn.edu>
2025-03-24 23:56:14 -0400

    Deleting test statements

--- main.py
+++ main.py
@@ -4,11 +4,3 @@ import time
 import random
 import json
 print("Hello World")
-print("This is a test")
-print("This is a test 2")
-print("This is a test 3")
-print("This is a test 4")
-print("This is a test 5")
-print("This is a test 6")
-print("This is a test 7")
-print("This is a test 8")
