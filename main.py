7089b4a9495e28f8fba7359941e57c4007e3d0c8
Daniyah Hasan <daniahsn@seas.upenn.edu>
2025-03-24 23:53:57 -0400

    Adding more test statements

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ import random
 import json
 print("Hello World")
 print("This is a test")
+print("This is a test 2")
+print("This is a test 3")
+print("This is a test 4")
+print("This is a test 5")
+print("This is a test 6")
+print("This is a test 7")
+print("This is a test 8")
